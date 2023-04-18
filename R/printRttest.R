743fc129af9eea23c6295a9f448dee163fa3c358
tuckwise <tcapps99@ou.edu>
2023-04-17 19:21:54 -0500

    print.Rttest documentation

--- R/printRttest.R
+++ R/printRttest.R
@@ -1,3 +1,12 @@
+#' print.Rttest
+#'
+#' @param x an object of class Rttest
+#' @param ... anything else
+#'
+#' @return returns the data frame as a kable table, as well as various parameters (p-value, alpha, confidenc interval)
+#' @export
+#'
+#' @examples \dontrun{print.Rttest(x)}
 print.Rttest <- function(x, ...) {
   cat("\nData Frame:\n")
   print(kableExtra::kable(x$data, format = 'simple'))
