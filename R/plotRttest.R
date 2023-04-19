f70e9642f76d33730f5c3d198f1ccffbd901fea0
tuckwise <tcapps99@ou.edu>
2023-04-19 16:35:18 -0500

    update documentation

--- R/plotRttest.R
+++ R/plotRttest.R
@@ -5,18 +5,28 @@
 #'
 #' @return prints out a side-by-side boxplot of the variables in the Rttest dataframe
 #' @export
+#' @importFrom ggplot2 ggplot aes geom_boxplot labs theme_minimal scale_fill_manual
+#' @importFrom dplyr %>%
+#' @importFrom tidyr gather
+#' @importFrom rlang sym
 #'
-#' @examples \dontrun{plot(x)}
+#' @examples \dontrun{plot(x, ...)}
 plot.Rttest <- function(x, ...) {
-  df_long <- x$data %>%
-    tidyr::gather(variable, value, X, Y) %>%
-    dplyr::filter(!is.na(value))
+  variable <- rlang::sym("variable")
+  value <- rlang::sym("value")
+  X <- rlang::sym("X")
+  Y <- rlang::sym("Y")
 
-  boxplot_gg <- ggplot(df_long, aes(x = variable, y = value, fill = variable)) +
-    geom_boxplot() +
-    labs(x = "Variable", y = "Value") +
-    theme_minimal() +
-    scale_fill_manual(values = c("X" = "steelblue", "Y" = "darkorange"))
+  df_long <- x$data %>%
+    tidyr::gather(!!variable, !!value, !!X, !!Y) %>%
+    dplyr::filter(!is.na(!!value))
+
+  boxplot_gg <- ggplot2::ggplot(df_long, ggplot2::aes(x = !!variable, y = !!value, fill = !!variable)) +
+    ggplot2::geom_boxplot() +
+    ggplot2::labs(x = "Variable", y = "Value") +
+    ggplot2::theme_minimal() +
+    ggplot2::scale_fill_manual(values = c("X" = "steelblue", "Y" = "darkorange"))
 
   print(boxplot_gg)
 }
+
